dd575bf7007115c0e6638d4672db489e365a39f0
Deeya99902 <deeyaphuyal13@gmail.com>
2025-08-07 16:53:14 +0545

    Initaial commit

---
+++ products/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import  reverse
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+     name = models.CharField(max_length=200,unique=True)   
+     description = models.TextField(blank=True)
+     price = models.DecimalField(max_digits=10, decimal_places=2)
+     Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+     image = models.ImageField(upload_to='products_pics',)
+     stock = models.PositiveIntegerField(default=0)
+     available = models.BooleanField(default=True)
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+     seller = models.ForeignKey(User, on_delete=models.CASCADE)
+     
+     def __str__(self):
+        return self.name
+    
+def get_absolute_url(self):
+    return reverse("product_details", kwargs={"pk": self.pk})
+    
+      
+
+    
+    
+    
+    
+   
+   
