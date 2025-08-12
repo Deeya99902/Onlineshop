7c96a18bbbf77b4eb3a2f7c15184a8f3a4e6c41b
Deeya99902 <deeyaphuyal13@gmail.com>
2025-08-12 17:00:36 +0545

    Initial commit

--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from .models import Product, Category
+from .models import Product, Category,seller
 from django.contrib.auth.decorators import login_required
 from .forms import ProductForm
 from django.contrib import messages
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
@@ -13,6 +14,45 @@ def home(request):
         'categories': categories,
     }
     return render (request,'',context)
+
+def product_list(request):
+    query = request.GET.get('q')
+    category_id = request.GET.get('category')
+    
+    products = Product.object.filter(available=True)
+    
+    if query:
+        products = Product.filter(
+            Q(name__icontains=query)|
+            Q(description__icontains=query)|
+            Q(seller_username__icontains=query)
+            
+        )
+        
+        
+    if category_id:
+        products = products.filter(category_id=category_id)
+        
+        
+        products = products.order_by('-created_at')
+        
+        paginator = paginator(products,12)
+        page = request.GET.get('page')
+        products = paginator.get_page(page)
+        
+        
+        categories = Category.objects.all()
+        
+        context = {
+            'products': products,
+            'categories': categories,
+            'query': query,
+            'select_category':int(category_id) if category_id else None,
+        
+        }
+        return render(request,'',context)
+        
+        
 
 
 @login_required 
@@ -44,6 +84,21 @@ def update_product(request,pk):
     else:
         form = ProductForm(instance=product)
     return render()
+
+@login_required
+def delete_product(request,pk):
+    product=get_object_or_404(Product,pk=pk,seller=request.user)
+    
+    if seller != request.user:
+        messages.error(request,"You cannot delete others product")
+        return redirect('')
+    
+    if request.method == 'POST':
+        product.delete()
+        messages.success(request,"Product deleted successfully")
+        return redirect('')
+    return render()
+
 
     
     
